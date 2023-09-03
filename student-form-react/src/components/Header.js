ce35c33a25085ee21f9acf67b2c8e51feaedd0dc
raksha <rakshar659@gmail.com>
2023-09-03 22:40:08 +0530

    failed

--- student-form-react/src/components/Header.js
+++ student-form-react/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="text-center">
       <h1 className="display-5 mb-5">
-        <strong>Student Database</strong>
+        <strong>Student Database gone Wrong</strong>
       </h1>
     </div>
   );
