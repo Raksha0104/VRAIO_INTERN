fba8a4bd41615f0c0c850f17d5223fcf8336988f
raksha <rakshar659@gmail.com>
2023-09-30 00:32:44 +0530

    modified

--- todo/src/Components/rightComponents/RightComp.js
+++ todo/src/Components/rightComponents/RightComp.js
@@ -1,13 +1,59 @@
+import "../Styles/rightcomp.css";
+
 export default function RightComp() {
   return (
     <>
       <div className="col-auto col-md-4 col-lg-2">
         <section className=" bg-dark text-white min-vh-100">
-          <div className="d-flex flex-row p-2">
-            <div className="item text-center mt-4 mx-3">
-              <h5 className="mx-4">Hii, User!</h5>
+          <div className="d-flex flex-row justify-content-between p-2">
+            <div className="d-flex flex-row">
+              <div className="item text-center mt-3 mx-3">
+                <h5 className="mx-4 py-2">Hii, User!</h5>
+              </div>
+              <div className="my-3">
+                <img
+                  src="../user.png"
+                  alt="..."
+                  className="img-thumbnail bg-dark"
+                ></img>
+              </div>
             </div>
           </div>
+          <hr className="mt-0" />
+          <div className="d-flex flex-row justify-content-between px-2 mx-2">
+            <div>
+              <h6>Darkmode</h6>
+            </div>
+            <div>
+              <label className="toggle-switch">
+                <input
+                  type="checkbox"
+                  // checked={isToggled}
+                  // onChange={handleToggle}
+                />
+                <span className="slider"></span>
+              </label>
+            </div>
+          </div>
+          <div className="d-flex flex-row justify-content-between px-2 mx-1 mt-2">
+            <div>
+              <h6>All Tasks </h6>
+            </div>
+            <div>
+              <h6>2/3</h6>
+            </div>
+          </div>
+          <div
+            className="progress justify-content-betwee mt-1 mx-2"
+            role="progressbar"
+            aria-label="Progress"
+            aria-valuenow={75}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          >
+            <div className="progress-bar bg-info w-75"></div>
+          </div>
+          <hr className="my-4" />
         </section>
       </div>
     </>
